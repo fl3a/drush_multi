04d1a8c8e7611d140c04e473a21edb6f79ec9495
Florian Latzel <fl3a@51103.no-reply.drupal.org>
2010-05-04 00:06:41 +0000

    Added short descriptions to page "cmds" (Commands).

--- doxygen/doxygen.php
+++ doxygen/doxygen.php
@@ -5,7 +5,7 @@
   
 /**
  * @file
- * Doxygen documentation 
+ * Additional Doxygen documentation 
  */
   
 /** 
@@ -94,21 +94,36 @@
 
 /**
  * @page cmds Commands  
+ * 
  * Drush_multi provides the following commands:
  * 
  * @subpage multi_status_help "multi-status"
- * 
+ *  
+ * An extended drush core status
+ *  
  * @subpage multi_create_help "multi-create"
  * 
+ * Creates a multisite installation
+ *  
  * @subpage multi_exec_help "multi-exec"
  * 
+ * Execute drush core commands on all sites (batch mode)
+ *  
  * @subpage multi_drupalupdate_help "multi-drupalupdate"
  * 
+ * Updates the installation if there is a new minor release available
+ *  
  * @subpage multi_sql_dump_help "multi-sql-dump"
  * 
+ * Execute a sql dump on all sites (batch mode)
+ *  
  * @subpage multi_nagios_help "multi-nagios"
  * 
+ * Command for use as nagios plugin with exit status and status message
+ *  
  * @subpage multi_site_help "multi-site"
+ * 
+ * Creates a site within the installation
  */
  
 /**
